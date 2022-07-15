5235c5ea02194d7b6cb1c44639f24171a0b2ad17
dukevaishnav1000 <dukevaishnav3853@gmail.com>
2022-07-15 07:28:36 +0530

    newscathcerapi

--- Assignment_projects/TeaTime_mediastack.js
+++ Assignment_projects/TeaTime_mediastack.js
@@ -6,13 +6,13 @@ console.log("using rapid api");
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': config.apikey,
-		'X-RapidAPI-Host': 'google-news1.p.rapidapi.com'
+        
+		'x-api-key': config.apikey
 	}
 };
 // var pageno=1;
  //var homeNewsUrl=`http://api.mediastack.com/v1/news?access_key=${api_key}&sources=bbc,cnn&limit=100`;
-var homeNewsUrl='https://google-news1.p.rapidapi.com/top-headlines?country=US&lang=en&limit=50';
+var homeNewsUrl='https://api.newscatcherapi.com/v2/latest_headlines?countries=IN,US,AU,UK,RU&lang=en&page_size=100';
 var type="homeNews";
 var newsPointer=0;
 //localStorage.clear();
@@ -37,7 +37,7 @@ if(!x){
         console.log(data);
         localStorage.setItem(type,JSON.stringify(data));
         console.log(localStorage.getItem(type));
-        //loadNews(); 
+        loadNews(); 
     })
     // .catch(error=>{
     //     console.log("error occured while storing in localStorage "+error);
@@ -47,21 +47,21 @@ if(!x){
 //console.log("today's date = ",todays.toDateString());
 }
 
-// function loadNews(){
-//     let newsobj=JSON.parse(localStorage.getItem(type));
-//     let sampleCard;
-//     let temp;
-//     for(let i=newsPointer;i<=newsPointer+24;i++)
-//     {
-//         sampleCard=main_row[0].children[0].cloneNode(true);
-//         sampleCard.children[0].children[0].src=newsobj["data"][i]["image"];
-//         temp=sampleCard.children[0].children[2].children[0].children[0]; // news title box
-//         temp.innerHTML=newsobj["data"][i]["title"]; // news title 
-//         temp.href=newsobj["data"][i]["url"];    // url hidden in news title
-//         main_row[0].appendChild(sampleCard);
-//     }
-//     newsPointer+=25;
-// }
+function loadNews(){
+    let newsobj=JSON.parse(localStorage.getItem(type));
+    let sampleCard;
+    let temp;
+    for(let i=newsPointer;i<=newsPointer+24;i++)
+    {
+        sampleCard=main_row[0].children[0].cloneNode(true);
+        sampleCard.children[0].children[0].src=newsobj["articles"][i]["media"];
+        temp=sampleCard.children[0].children[2].children[0].children[0]; // news title box
+        temp.innerHTML=newsobj["articles"][i]["title"]; // news title 
+        temp.href=newsobj["articles"][i]["link"];    // url hidden in news title
+        main_row[0].appendChild(sampleCard);
+    }
+    newsPointer+=25;
+}
 
 
 
